1776938780dd6a8ea85e2480fd4e5898b5ee7fae
Andrew Allison <andrew.allison@xdesign.com>
2023-11-08 10:21:02 +0000

    Tweak cucumber location

--- cucumber.js
+++ cucumber.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-undef
 let common = [
-  'features/**/*.feature', // Specify our feature files
+  '__tests__/features/**/*.feature', // Specify our feature files
   '--require-module ts-node/register', // Load TypeScript module
-  '--require features/**/*.ts', // Load step definitions
+  '--require __tests__/features/**/*.ts', // Load step definitions
   '--format progress-bar', // Load custom formatter
   '--format html:./output/reports/cucumber-report.html', // Load custom formatter
 ].join(' ');
